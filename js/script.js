f159368c5120f0e0c08304fab468a2fc218fd5a7
owner <alexandr.nekto@gmail.com>
2022-07-20 14:50:51 +0300

    prototipe

--- js/script.js
+++ js/script.js
@@ -374,92 +374,101 @@
 
 //Урок 22
 
-let a = 5,
-    b = a;
+// let a = 5,
+//     b = a;
 
-b = b + 5;
-console.log(b);
-console.log(a);
+// b = b + 5;
+// console.log(b);
+// console.log(a);
 
-//передача по ссылке
-const obj = {
-    a: 5,
-    b: 1
-};
-// const copy = obj;
+// //передача по ссылке
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+// // const copy = obj;
 
-// copy.a = 10;
+// // copy.a = 10;
 
-// console.log(copy);
-// console.log(obj);
-
-
-function copy(mainObj) {
-    let objCopy = {};
-    let key;
-    for (key in mainObj) {
-        objCopy[key] =  mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y:4
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-};
-const clone = (Object.assign({}, add));
-
-clone.d = 20;
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-newArray[0] = 'asasas';
-console.log(newArray);
-console.log(oldArray);
+// // console.log(copy);
+// // console.log(obj);
 
 
-const video = ['ytobe', 'vimeo', 'rutybe' ],
-      blogs = ['wordpress', 'livejornal', 'blogg'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-      console.log(internet);
+// function copy(mainObj) {
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] =  mainObj[key];
+//     }
+//     return objCopy;
+// }
 
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-const num = [2, 5, 7];
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y:4
+//     }
+// };
 
-log(...num);
+// const newNumbers = copy(numbers);
 
-const array = ['a', 'b'];
-const newArray = [...array];
+// newNumbers.a = 10;
+// // console.log(newNumbers);
+// // console.log(numbers);
 
-const q = {
-    one: 1,
-    two: 2
-};
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+// const clone = (Object.assign({}, add));
 
-const newObj= {...q};
+// clone.d = 20;
+// // console.log(add);
+// // console.log(clone);
 
-newObj.one = 3;
-console.log(newObj);
-console.log(q);
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+// newArray[0] = 'asasas';
+// console.log(newArray);
+// console.log(oldArray);
 
+
+// const video = ['ytobe', 'vimeo', 'rutybe' ],
+//       blogs = ['wordpress', 'livejornal', 'blogg'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+//       console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+// const newArray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj= {...q};
+
+// newObj.one = 3;
+// console.log(newObj);
+// console.log(q);
+
+//Урок 23
+
+let str = 'some';
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
